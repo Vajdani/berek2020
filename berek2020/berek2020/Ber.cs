9bbe71b7b293814fc233d524ebf8a055decc5b27
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-11-18 09:17:39 +0100

    Add files via upload

---
+++ berek2020/berek2020/Ber.cs
@@ -0,0 +1,21 @@
+﻿namespace berek2020
+{
+    internal class Ber
+    {
+        public string Nev { get; set; }
+        public bool Nem { get; set; }
+        public string Reszleg { get; set; }
+        public string Belepes { get; set; }
+        public int Fizetes { get; set; }
+
+        public Ber(string line)
+        {
+            string[] spli = line.Split(';');
+            Nev = spli[0];
+            Nem = spli[1] == "férfi";
+            Reszleg = spli[2];
+            Belepes = spli[3];
+            Fizetes = int.Parse(spli[4]);
+        }
+    }
+}
---
+++ berek2020/berek2020/Program.cs
@@ -0,0 +1,37 @@
+﻿using berek2020;
+
+List<Ber> berek = [];
+using StreamReader r = new("../../../src/berek2020.txt");
+r.ReadLine();
+while (!r.EndOfStream)
+{
+    berek.Add(new Ber(r.ReadLine()!));
+}
+
+Console.WriteLine($"3. feladat: {berek.Count} dolgozó van az állományban.");
+Console.WriteLine($"4. feladat: {Math.Round(berek.Average(b => b.Fizetes) / 1000, 1)} eFt");
+
+Console.Write("5. feladat: Adja meg a részleg nevét: ");
+string reszleg = Console.ReadLine()!;
+var reszlegBerek = berek.Where(b => b.Reszleg == reszleg);
+if (!reszlegBerek.Any())
+{
+    Console.WriteLine("6. feladat: A megadott részleg nem létezik a cégnél!");
+}
+else
+{
+    Ber legnagyobb = reszlegBerek.MaxBy(b => b.Fizetes)!;
+    Console.WriteLine($"6. feladat: A legtöbbet kereső dolgozó a megadott részlegen");
+    Console.WriteLine($"\tNév: {legnagyobb.Nev}");
+    Console.WriteLine($"\tNem: {(legnagyobb.Nem ? "férfi" : "nő")}");
+    Console.WriteLine($"\tBelépés: {legnagyobb.Belepes}");
+    Console.WriteLine($"\tBér: {legnagyobb.Fizetes} Ft");
+}
+
+Console.WriteLine("7. feladat: Statisztika");
+var stat = berek.GroupBy(b => b.Reszleg).OrderBy(g => g.Key);
+int spacing = stat.Max(g => g.Key.Length);
+foreach (var item in stat)
+{
+    Console.WriteLine($"\t{item.Key.PadRight(spacing)} - {item.Count()} fő");
+}
